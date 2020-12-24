5b2d33fc214ec718d7cd21eb39ab7091384f45bf
Manglemix <shabouza030@gmail.com>
2020-12-24 21:30:00 +0800

    Added blending to CharacterAnimator

--- trokara_scripts/character_animator.gd
+++ trokara_scripts/character_animator.gd
@@ -3,6 +3,7 @@ extends AnimationPlayer
 
 
 export var character_jump_path: NodePath
+export var blending := 0.2
 
 var _override_priority: int
 
@@ -11,7 +12,7 @@ onready var character_jump: CharacterJump = get_node(character_jump_path)
 
 
 func _ready():
-	character_jump.connect("jumped", self, "override_play", ["jump-loop"])
+	character_jump.connect("jumped", self, "override_play", ["jump-loop", blending])
 
 
 func override_play(anim: String, override_priority:=0, restart:=true) -> void:
@@ -28,7 +29,7 @@ func override_play(anim: String, override_priority:=0, restart:=true) -> void:
 func _process(_delta):
 	if character.is_on_floor():
 		if is_zero_approx(character.movement_vector.length_squared()):
-			play("idle-loop")
+			play("idle-loop", blending)
 		
 		else:
-			play("run-loop")
+			play("run-loop", blending)
