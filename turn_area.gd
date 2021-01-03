85050b537a08e0e25f791343206e97333e391ba8
Manglemix <shabouza030@gmail.com>
2021-01-03 18:59:46 +0800

    Refactored how turn areas and buttons work

--- turn_area.gd
+++ turn_area.gd
@@ -2,20 +2,66 @@ class_name TurnArea
 extends Area
 
 
+export var glide_time := 0.3
 export var rotations: PoolRealArray
 
 
 func _ready():
+	set_process_input(false)
 	connect("body_entered", self, "_handle_body_entered")
 	connect("body_exited", self, "_handle_body_exited")
 
 
 func _handle_body_entered(body: Node) -> void:
 	if body == GlobalStuff.player:
-		GlobalStuff.ui.turn.turn_area = self
+		set_process_input(true)
 		GlobalStuff.ui.turn.show()
 
 
 func _handle_body_exited(body: Node) -> void:
 	if body == GlobalStuff.player:
+		set_process_input(false)
 		GlobalStuff.ui.turn.hide()
+
+
+func _input(event):
+	if event.is_action_pressed("transition"):
+		var diff := INF
+		var idx: int
+		var original_angle: float = GlobalStuff.camera_base.rotation_degrees.y
+		
+		for i in range(rotations.size()):
+			var tmp_diff := rotations[i] - original_angle
+			
+			if abs(tmp_diff) <= abs(diff):
+				diff = tmp_diff
+				idx = i
+		
+		var angle: float
+		idx += 1
+		
+		if idx == rotations.size():
+			angle = rotations[0]
+		
+		else:
+			angle = rotations[idx]
+		
+		var new_transform := Transform.IDENTITY.rotated(Vector3.UP, deg2rad(angle))
+		new_transform.origin = GlobalStuff.camera_base.global_transform.origin
+		var tween := Tween.new()
+		tween.interpolate_property(GlobalStuff.camera_base, NodePath("global_transform"), GlobalStuff.camera_base.global_transform, new_transform, glide_time, Tween.TRANS_QUAD)
+		GlobalStuff.camera_base.add_child(tween)
+		tween.start()
+		
+		new_transform = new_transform.rotated(Vector3.UP, PI / 2)
+		new_transform.origin = global_transform.origin
+		tween = Tween.new()
+		tween.interpolate_property(GlobalStuff.player, NodePath("global_transform"), GlobalStuff.player.global_transform, new_transform, glide_time, Tween.TRANS_QUAD)
+		GlobalStuff.player.add_child(tween)
+		tween.start()
+		
+		var movement : ControllableCharacterMovement = GlobalStuff.player.get_node("ControllableCharacterMovement")
+		movement.targeting = false
+		yield(tween, "tween_completed")
+		movement.reset_target_basis()
+		movement.targeting = true
--- ui/turn_button.gd
+++ ui/turn_button.gd
@@ -1,10 +1,6 @@
 extends Control
 
 
-export var glide_time := 0.3
-
-var turn_area: TurnArea
-
 var _index: int
 var _mouse_clicked := false
 
@@ -18,6 +14,7 @@ func _input(event):
 	if event is InputEventScreenTouch and not event.pressed and event.index == _index:
 		_mouse_clicked = false
 		set_process_input(false)
+		GlobalStuff.trigger_event("transition", false)
 
 
 func _gui_input(event):
@@ -26,48 +23,7 @@ func _gui_input(event):
 	
 	elif event is InputEventScreenTouch and event.pressed:
 		get_tree().set_input_as_handled()
+		GlobalStuff.trigger_event("transition", true)
 		_mouse_clicked = true
 		_index = event.index
 		set_process_input(true)
-		event = InputEventAction.new()
-		
-		var diff := INF
-		var idx: int
-		var original_angle: float = GlobalStuff.camera_base.rotation_degrees.y
-		var rotations := turn_area.rotations
-		
-		for i in range(rotations.size()):
-			var tmp_diff := rotations[i] - original_angle
-			
-			if abs(tmp_diff) <= abs(diff):
-				diff = tmp_diff
-				idx = i
-		
-		var angle: float
-		idx += 1
-		
-		if idx == rotations.size():
-			angle = rotations[0]
-		
-		else:
-			angle = rotations[idx]
-		
-		var transform := Transform.IDENTITY.rotated(Vector3.UP, deg2rad(angle))
-		transform.origin = GlobalStuff.camera_base.global_transform.origin
-		var tween := Tween.new()
-		tween.interpolate_property(GlobalStuff.camera_base, NodePath("global_transform"), GlobalStuff.camera_base.global_transform, transform, glide_time, Tween.TRANS_QUAD)
-		GlobalStuff.camera_base.add_child(tween)
-		tween.start()
-		
-		transform = transform.rotated(Vector3.UP, PI / 2)
-		transform.origin = turn_area.global_transform.origin
-		tween = Tween.new()
-		tween.interpolate_property(GlobalStuff.player, NodePath("global_transform"), GlobalStuff.player.global_transform, transform, glide_time, Tween.TRANS_QUAD)
-		GlobalStuff.player.add_child(tween)
-		tween.start()
-		
-		var movement : ControllableCharacterMovement = GlobalStuff.player.get_node("ControllableCharacterMovement")
-		movement.targeting = false
-		yield(tween, "tween_completed")
-		movement.reset_target_basis()
-		movement.targeting = true
